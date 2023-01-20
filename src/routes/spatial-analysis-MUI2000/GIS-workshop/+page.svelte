9d49e581c84570032fb807fd32aba55465b7636b
Jeff Allen <jeffallen89@gmail.com>
2023-01-20 10:25:12 -0500

    adding in some text

--- src/routes/spatial-analysis-MUI2000/GIS-workshop/+page.svelte
+++ src/routes/spatial-analysis-MUI2000/GIS-workshop/+page.svelte
@@ -39,16 +39,25 @@
 
         <h2>(Q)GIS WORKSHOP</h2>
 
-        <p>January 23/30, 2023</p>
+        <p>Jeff Allen</p>
+
+        <p>January 23 & 30, 2023</p>
 
     </div>
 
     <div id="text">
 
-        <p>
-
-
-        </p>
+        <p>We use Geographic Information Systems (GIS) to analyze, manipulate, and visualize spatial information on a computer.</p>
+        <p>Why is GIS useful?</p>
+            <ul class="list">
+            <li>visualize spatial data</li>
+            <li>explore spatial patterns and relationships</li>
+            <li>make pretty and useful maps</li>
+            <li>generate new data, either &quot;by hand&quot; or via spatial relationships from other data (e.g. through spatial queries)</li>
+            <li>perform spatial analysis (i.e. statistical methods applied to spatial data)</li>
+            </ul>
+        <p>GIS is often thought of as more than just a tool or piece of software. It can refer to all aspects of managing and analyzing digital spatially referenced data.</p>
+        <p>The following provides a brief overview of GIS followed by two short tutorials for getting started with GIS, using <a href="https://www.qgis.org/en/site/">QGIS</a>, a free and open source desktop GIS software. The first tutorial uses data from QQQQQQQQ. The second analyzes demographic data from the Canadian census as well as public transit data for Toronto.</p>
 
 
     </div>
