c1a993f26c97b9b6f4aad4ccce26a962db5cad55
Jeff Allen <jeffallen89@gmail.com>
2023-01-30 10:17:21 -0500

    minor copy edits

--- src/routes/spatial-analysis-MUI2000/GIS-workshop/+page.svelte
+++ src/routes/spatial-analysis-MUI2000/GIS-workshop/+page.svelte
@@ -237,7 +237,6 @@
         <ul>
             <li><a href="https://open.toronto.ca/dataset/toronto-public-library-branch-locations/" target="_blank">Public Libraries</a>.</li>
             <li><a href="https://github.com/schoolofcities/mapping-workshops-2023/raw/main/data/toronto-apartments.geojson" target="_blank">Apartment Buildings</a> (originally geocoded from the City's apartment building registration and evaluation data on <a href="https://open.toronto.ca/catalogue/?search=apartment%20buildings&sort=score%20desc" target="_blank">Open Data Toronto</a>).</li>
-            <li><a href="https://open.toronto.ca/dataset/business-improvement-areas/" target="_blank">Business Improvement Areas (BIA)</a> (same link as in Tutorial 1)</li>
         </ul>
 
         <h4>Projections and re-projecting data</h4>
@@ -266,11 +265,12 @@
 
         <p>
             Above we were able to select features in a layer based on their spatial location relative to another layer. We can also use spatial location to join and aggregate data between layers. Let's use two new datasets for an example. Outdoor Ice Rinks in Toronto, and polygons representing the 6 former municipalities.
-
+        </p>
             <ul>
                 <li><a href="https://open.toronto.ca/dataset/former-municipality-boundaries/" target="_blank">Former Municipal Boundaries</a>.</li>
                 <li><a href="https://open.toronto.ca/dataset/outdoor-artificial-ice-rinks/" target="_blank">Outdoor Ice Rinks</li>
             </ul>
+        <p>
             We can use a spatial join to count how many outdoor rinks are within each former municipality. Click on <i>Processing</i> in the top bar, then click <i>Toolbox</i>. A panel should open up, which includes almost all of the built in geoprocessing tools included in QGIS. To attempt a spatial join, search for and open <i>Join attributes by location (summary)</i>. We want to join to features in the boundaries layer by comparing to the outdoor rinks layer. Then click on <i>Summaries to Calculate</i>. Here we can select what statistics to calculate. For this example, we just want to select <i>count</i>. Then click run. The output will be a new layer, but with additional attribute columns with counts of rinks. Open up the attribute table to see.
         </p>
         <img src={imgQgisCountTable} alt="qgis-counttable">
